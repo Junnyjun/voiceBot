9cd31fc98f9f4e2500e345edc1f6b77eef5d47d2
Junnyjun <chbe5082@naver.com>
2025-02-28 20:35:35 +0900

    init

---
+++ ai/src/test/kotlin/io/junnyland/ai/AiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.junnyland.ai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
