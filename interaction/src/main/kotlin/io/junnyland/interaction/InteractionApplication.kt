bd8b78ff8cbd248893d68fc7cb70f3f6eccfbca9
Junnyjun <chbe5082@naver.com>
2025-03-01 20:36:50 +0900

    init

---
+++ interaction/src/main/kotlin/io/junnyland/interaction/InteractionApplication.kt
@@ -0,0 +1,11 @@
+package io.junnyland.interaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InteractionApplication
+
+fun main(args: Array<String>) {
+    runApplication<InteractionApplication>(*args)
+}
---
+++ interaction/src/test/kotlin/io/junnyland/interaction/InteractionApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.junnyland.interaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InteractionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
